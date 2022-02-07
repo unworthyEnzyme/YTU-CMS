4bae8e7a792b2a15ea4d41b3974e91dc44722308
unworthyEnzyme <unworthy_enzyme@protonmail.com>
2022-02-07 20:25:43 +0300

    Added Sidebar component to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
+import Sidebar from "./components/Sidebar";
+
 function App() {
   return (
-    <div>hello world</div>
+    <div className="flex w-screen h-screen">
+      <Sidebar/>
+    </div>
   )
 }
 
