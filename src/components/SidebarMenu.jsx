59dac8af5a26b6d3280de7b2f13bb52de4caba19
unworthyEnzyme <unworthy_enzyme@protonmail.com>
2022-02-07 20:25:11 +0300

    add SidebarMenu component

---
+++ src/components/SidebarMenu.jsx
@@ -0,0 +1,11 @@
+export default function SidebarMenu() {
+  
+  return (
+    <div className="flex flex-col gap-2 justify-center items-center text-white mt-40">
+      <div>Evraklar</div>
+      <div>Etkinlikler</div>
+      <div>Profili Duzenle</div>
+      {/* <div>Klup Profili Duzenler</div> */}
+    </div>
+  )
+}
