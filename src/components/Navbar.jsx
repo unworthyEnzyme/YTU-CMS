68cb021b98658f74030f8ef67d1b7856f036548a
unworthyEnzyme <unworthy_enzyme@protonmail.com>
2022-02-07 21:37:05 +0300

    changed the styles a little bit
    Navbar items are too far away from each so i
    bring them together and add a gap

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import NavbarItem from "./NavbarItem";
 export default function Navbar() {
 
   return (
-    <div className="flex w-full h-16 bg-slate-200 justify-around items-end">
+    <div className="flex w-full h-16 bg-slate-200 justify-center gap-20 items-end">
       <NavbarItem itemName="Duzenlenenler" url="test"/>
       <NavbarItem itemName="Onay Bekleyenler" url="test"/>
       <NavbarItem itemName="Onaylanmis" url="test"/>
