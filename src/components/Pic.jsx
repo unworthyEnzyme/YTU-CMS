677509a74aa8cb892710537525907ee13b43cb69
unworthyEnzyme <unworthy_enzyme@protonmail.com>
2022-02-07 20:24:44 +0300

    add Pic component

---
+++ src/components/Pic.jsx
@@ -0,0 +1,7 @@
+export default function Pic(picUrl) {
+  return (
+    <div className="flex justify-center items-center">
+      <div className="w-10 h-10 rounded-full bg-white my-3"></div> 
+    </div>
+  );
+}
