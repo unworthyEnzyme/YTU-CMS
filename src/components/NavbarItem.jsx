e30aa053bd9a0b7025184e21c76ae6ecd292a63d
unworthyEnzyme <unworthy_enzyme@protonmail.com>
2022-02-08 01:22:13 +0300

    i draw the underline only for the active route

--- src/components/NavbarItem.jsx
+++ src/components/NavbarItem.jsx
@@ -1,9 +1,21 @@
-export default function NavbarItem({ itemName }) {
+import { useLocation } from "react-router-dom";
 
-  return (
-    <div>
-      <div className="m-1">{itemName}</div>
-      <div className="w-full bg-slate-900 h-1"></div>
-    </div>
-  )
+export default function NavbarItem({ itemName, url }) {
+  const currentUrl = useLocation().pathname.split('/')[2]
+  if (url === currentUrl) {
+    return (
+      <div>
+        <div className="m-1">{itemName}</div>
+        <div className="w-full bg-red-300 h-1"></div>
+      </div>
+    )
+  } else {
+    return (
+      <div>
+        <div className="m-1">{itemName}</div>
+        <div className="w-full bg-blue-300 h-1"></div>
+      </div>
+    )
+  }
+  
 }
