79e86077d30b2e7ea39f4eec98717a7d1e41f7f3
unworthyEnzyme <unworthy_enzyme@protonmail.com>
2022-02-08 01:22:58 +0300

    pull the data from the server with the useFetch hook

--- src/components/EventCards.jsx
+++ src/components/EventCards.jsx
@@ -1,6 +1,12 @@
 import EventCard from "./EventCard";
+import { useLocation } from "react-router-dom";
+import { useEffect, useState } from "react";
+import useFetch from "../hooks/useFetch";
 
 export default function EventCards() {
+  const currentUrl = useLocation().pathname.split('/')[2]
+  const data = useFetch("http://localhost:3001/duzenlenenler")
+  console.log(data)
   return (
     <div className="flex justify-center items-center m-6">
       <div className="grid grid-cols-3">
