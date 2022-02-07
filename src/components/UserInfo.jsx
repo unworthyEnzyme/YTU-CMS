4623a7d40891d535eae3d1ec4e9de23d57f24ed1
unworthyEnzyme <unworthy_enzyme@protonmail.com>
2022-02-07 20:24:57 +0300

    Add UserInfo  component

---
+++ src/components/UserInfo.jsx
@@ -0,0 +1,9 @@
+export default function UserInfo() {
+
+  return (
+    <div className="flex flex-col gap-2 justify-center items-center text-white">
+      <div>Emircan</div>
+      <div>Baskan Yardimcisi</div>
+    </div>
+  )
+}
