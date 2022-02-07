cd8a3a72d80b2a751315aa7170509196831ee243
unworthyEnzyme <unworthy_enzyme@protonmail.com>
2022-02-07 20:25:21 +0300

    add Sidebar component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,1 +1,13 @@
+import Pic from "./Pic";
+import SidebarMenu from "./SidebarMenu";
+import UserInfo from "./UserInfo";
 
+export default function Sidebar() {
+  return (
+    <div className="h-full w-1/4 bg-slate-700 text-lg">
+      <Pic />
+      <UserInfo />
+      <SidebarMenu />
+    </div>
+  );
+}
