2bf76cfad26162a72e9eaf8cf01d217698d79b14
unworthyEnzyme <unworthy_enzyme@protonmail.com>
2022-02-07 20:40:39 +0300

    added gaps between menu items

--- src/components/SidebarMenu.jsx
+++ src/components/SidebarMenu.jsx
@@ -1,7 +1,7 @@
 export default function SidebarMenu() {
   
   return (
-    <div className="flex flex-col gap-2 justify-center items-center text-white mt-40">
+    <div className="flex flex-col gap-4 justify-around items-center text-white mt-40">
       <div className="hover:cursor-pointer hover:text-slate-400">Evraklar</div>
       <div className="hover:cursor-pointer hover:text-slate-400">Etkinlikler</div>
       <div className="hover:cursor-pointer hover:text-slate-400">Profili Duzenle</div>
