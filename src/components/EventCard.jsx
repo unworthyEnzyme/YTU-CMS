ef2c4835ff209c30335129a9a4d094e5bd8eadcc
unworthyEnzyme <unworthy_enzyme@protonmail.com>
2022-02-08 22:35:52 +0300

    Card' in ustune gelince zipliyo

--- src/components/EventCard.jsx
+++ src/components/EventCard.jsx
@@ -10,7 +10,7 @@ export default function EventCard({ name, description }) {
   const showButtons = currentUrl === "duzenlenenler"
 
   return (
-    <div className="w-52 h-52 rounded-md bg-slate-200 m-4 break-words flex flex-col justify-between ">
+    <div className="w-52 h-52 rounded-md bg-slate-200 m-4 break-words flex flex-col justify-between shadow-xl hover:shadow-none transition-all ease-in-out hover:cursor-pointer duration-500 hover:scale-95">
       <div className="m-6">
         <div className="text-xl">{name}</div>
         <div className="text-sm">{description}</div>
