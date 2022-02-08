59d410c98bcfbbffd510648d3bebbe5bbe97ffdc
armagann12 <armagansisik@gmail.com>
2022-02-08 15:21:34 +0300

    Updated navbar dynamic data

--- src/components/EventCards.jsx
+++ src/components/EventCards.jsx
@@ -5,18 +5,82 @@ import useFetch from "../hooks/useFetch";
 
 export default function EventCards() {
   const currentUrl = useLocation().pathname.split('/')[2]
-  const data = useFetch("http://localhost:3001/duzenlenenler")
-  console.log(data)
+  const duzenlenenlerData = useFetch("http://localhost:3001/duzenlenenler")
+  const onayBekleyenlerData = useFetch("http://localhost:3001/onayBekleyenler")
+  const onaylanmisData = useFetch("http://localhost:3001/onaylanmis")
+
+  if (currentUrl === "duzenlenenler"){
+    return(
+      <div className="flex justify-center items-center m-6">
+        <div className="grid grid-cols-3">
+          {duzenlenenlerData.map(info => (
+            <div>
+              <EventCard name={info.name} description={info.description} />
+            </div>
+          ))}
+        </div>
+      </div>
+    )
+  }
+
+  if (currentUrl === "onay-bekleyenler"){
+    return(
+      <div className="flex justify-center items-center m-6">
+        <div className="grid grid-cols-3">
+          {onayBekleyenlerData.map(info => (
+            <div>
+              <EventCard name={info.name} description={info.description} />
+            </div>
+          ))}
+        </div>
+      </div>
+    )
+  } 
+
+  if (currentUrl === "onaylanmis"){
+    return(
+      <div className="flex justify-center items-center m-6">
+        <div className="grid grid-cols-3">
+          {onaylanmisData.map(info => (
+            <div>
+              <EventCard name={info.name} description={info.description} />
+            </div>
+          ))}
+        </div>
+      </div>
+    )
+  }
+
   return (
     <div className="flex justify-center items-center m-6">
       <div className="grid grid-cols-3">
-        <EventCard name="Hardlev" description="lorem ipsum so" />
-        <EventCard name="Hardlev" description="lorem ipsum so" />
-        <EventCard name="Hardlev" description="lorem ipsum so" />
-        <EventCard name="Hardlev" description="lorem ipsum so" />
-        <EventCard name="Hardlev" description="lorem ipsum so" />
-        <EventCard name="Hardlev" description="lorem ipsum so" />
       </div>
     </div>
   );
 }
+
+
+/*
+
+{duzenlenenlerData.map(info => (
+          <div>
+            <EventCard name={info.name} description={info.description} />
+          </div>
+        ))}
+
+        {onayBekleyenlerData.map(info => (
+          <div>
+            <EventCard name={info.name} description={info.description} />
+          </div>
+        ))}
+
+
+      {onaylanmisData.map(info => (
+          <div>
+            <EventCard name={info.name} description={info.description} />
+          </div>
+        ))}
+
+*/
+
+// <EventCard name="Hardlev" description="lorem ipsum so" />
