fda2685d77fd2e2a69532a50269b49c223f0df2e
unworthyEnzyme <unworthy_enzyme@protonmail.com>
2022-02-08 19:56:49 +0300

    made a little animation with the cards

--- src/components/EventCard.jsx
+++ src/components/EventCard.jsx
@@ -3,7 +3,7 @@ import EditButton from "./EditButton";
 
 export default function EventCard({ name, description }) {
   return (
-    <div className="w-52 h-52 rounded-md bg-slate-200 m-4 break-words flex flex-col justify-between">
+    <div className="w-52 h-52 rounded-md bg-slate-200 m-4 break-words flex flex-col justify-between shadow-xl hover:shadow-none transition-all ease-in-out hover:cursor-pointer duration-500 hover:scale-95">
       <div className="m-6">
         <div className="text-xl">{name}</div>
         <div className="text-sm">{description}</div>
