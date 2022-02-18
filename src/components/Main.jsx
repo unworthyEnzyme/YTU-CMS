ec0abc82b256a6dcd2acf9e2f03471adf5107fba
armagann12 <armagansisik@gmail.com>
2022-02-19 01:25:09 +0300

    removed the unnecessary imports

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,13 +1,14 @@
 import { Navigate, Outlet, Route, Routes } from "react-router-dom";
+import { readAuthToken } from "../helpers/ReadorWriteAuthToken";
 import EventCards from "./EventCards";
 import Navbar from "./Navbar";
+import ProtectedComponent from "./ProtectedComponent";
 
 export default function Main() {
-
-  return (
-    <div className="flex flex-col w-3/4">
-      <Navbar/>
-      <Outlet/>
-    </div>
-  )
+    return (
+      <div className="flex flex-col w-3/4">
+        <Navbar />
+        <Outlet />
+      </div>
+    )
 }
