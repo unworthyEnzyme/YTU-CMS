942c06c4ed9f902e714a853cd06c2af4d11e2d3d
armagann12 <armagansisik@gmail.com>
2022-02-19 01:31:48 +0300

    Protect the app
    We are reading from the local storage using the
    helper function readAuthToken.
    If you dont have jwt token we are redirecting the client to login page.
    For now we use window.location object but this intoduces performans hit.
    Because we made another request to the server for the client side code.
    For now we dont care.
    It works for now.

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,32 @@ import Sidebar from "./components/Sidebar";
 import Main from "./components/Main"
 import Popup from "./components/Popup";
 import { RecoilRoot } from "recoil";
+import { useNavigate } from 'react-router-dom'
+import { readAuthToken } from "./helpers/ReadorWriteAuthToken";
+import { useEffect } from "react";
 
 function App() {
-  return (
-    <>
-      <RecoilRoot>
-        <div className="flex w-screen h-screen">
-        <Popup>
-          <div className="w-10 h-10 bg-white"></div>
-        </Popup>
-        <Sidebar/>
-        <Outlet/>
-      </div>
-      </RecoilRoot>
-    </>
-  )
+    const jwt = readAuthToken()
+    if (!jwt) {
+      window.location.assign("http://localhost:3000/login")
+      return(
+        <div>Redirecting</div>
+      )
+    } else {
+      return (
+        <>
+          <RecoilRoot>
+            <div className="flex w-screen h-screen">
+              <Popup>
+                <div className="w-10 h-10 bg-white"></div>
+              </Popup>
+              <Sidebar />
+              <Outlet />
+            </div>
+          </RecoilRoot>
+        </>
+      )
+    }
 }
 
 export default App
