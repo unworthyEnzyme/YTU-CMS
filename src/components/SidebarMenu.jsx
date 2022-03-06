83519b4386d6c7df33dc3acb84da4dfc772021dd
armagann12 <armagansisik@gmail.com>
2022-03-06 21:40:24 +0300

    Çıkış yap fonksiyonunu ekledim

--- src/components/SidebarMenu.jsx
+++ src/components/SidebarMenu.jsx
@@ -1,6 +1,10 @@
 import { Link } from "react-router-dom";
 
 export default function SidebarMenu() {
+  function cikisYap(){
+    window.localStorage.clear()
+    window.location.reload()
+  }
   return (
     <div className="flex flex-col gap-4 justify-around items-center text-white mt-40">
       <Link to="/evraklar">
@@ -19,7 +23,7 @@ export default function SidebarMenu() {
         </div>
       </Link>
       {/* <div>Klup Profili Duzenler</div> */}
-      <button className="text-red-300 hover:text-red-500">Cikis Yap</button>
+      <button className="text-red-300 hover:text-red-500" onClick={cikisYap}>Cikis Yap</button>
     </div>
   );
 }
